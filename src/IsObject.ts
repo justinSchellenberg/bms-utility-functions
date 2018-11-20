f204df8fd62bf73d7dbb8e7c508eb98cd606ce96
Justin Schellenberg <justin.schellenberg@boldcommerce.com>
2018-11-19 21:49:41 -0600

    0.0.6

--- src/IsObject.ts
+++ src/IsObject.ts
@@ -4,5 +4,5 @@
  * @returns {boolean} - Return true if variable is an object, else returns false
  */
 export default function isObject(variable: any): boolean {
-  return (typeof variable === "object" && !Array.isArray(variable) && variable !== null);
+  return typeof variable === 'object' && !Array.isArray(variable) && variable !== null;
 }
