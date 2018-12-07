d421d22b4185de332d5cb0303f1dc351b37504d4
Justin Schellenberg <justin.schellenberg@boldcommerce.com>
2018-12-06 23:02:57 -0600

    Update observe function again.

--- src/observables/observeElement.ts
+++ src/observables/observeElement.ts
@@ -1,17 +1,15 @@
-export interface IObserverConfig {
-  attributes: boolean;
-  characterData: boolean;
-  childList: boolean;
-  subtree: boolean;
-  attributeOldValue: boolean;
-  characterDataOldValue: boolean;
-  attributeFilter: boolean;
-}
-
+/**
+ *
+ * @param watchElement The Element To Observer
+ * @param {boolean} observeOnce - To Only observer the element once, if false will continue to observer
+ * @param callback - The callback function to execute every time the element changes.
+ * @constructor
+ */
 export const ObserveElement = (watchElement: any, observeOnce: boolean = true, callback: any) => {
   let observer: any;
   observer = new MutationObserver(observed);
-  const config: any = {
+
+  observer.observe(watchElement.parentNode, {
     attributeFilter: false, // What specific attributes should be watched?
     attributeOldValue: true, // Do you want the original value of the attribute?
     attributes: true, // Has the element had any changes to attributes?
@@ -19,8 +17,7 @@ export const ObserveElement = (watchElement: any, observeOnce: boolean = true, c
     characterDataOldValue: true, // Do you want the original text of the element?
     childList: true, // Have elements been added/removed directly in this element?
     subtree: true, // Have elements more than one level deep changed?
-  };
-  observer.observe(watchElement.parentNode, config);
+  });
 
   function observed(e: any) {
     if (e[0].removedNodes) {
